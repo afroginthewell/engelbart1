eb5e4bcdcbef4ee029b71cb772a2a944f6f7ea47
yuan <yuan@LAPTOP-EVUGTTJ4>
2019-05-22 09:26:06 +0800

    Alex, change some bugs in the ingredient maintain GUI

--- BrewDay/src/GUI/IngredientMantainGUI.java
+++ BrewDay/src/GUI/IngredientMantainGUI.java
@@ -38,9 +38,120 @@ public class IngredientMantainGUI extends JFrame{
 			}
 		};
 
+		JPanel emptyPanel = new JPanel();
+		emptyPanel.setLayout(new FlowLayout(1,10,10));
+		emptyPanel.setPreferredSize(new Dimension(400, 80));
+		///////////bg////////////
+
+		JPanel p = new JPanel();
+		p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
 		
+		JPanel p1 = new JPanel(); 
+		p1.setLayout(new FlowLayout(1,10,10));
+		JTextField title = new JTextField("Ingredient list");
+		title.setFont(new Font("Verdana",Font.ITALIC,20));
+		title.setBorder(BorderFactory.createEmptyBorder());
+		title.setOpaque(false);
+		title.setEditable(false);
+		
+		p1.add(title);
+		title.setEditable(false);
+		p.add(p1);
+
+		JPanel p2 = new JPanel();
+		p2.setLayout(new GridLayout(sIngredientList.size(), 1, 20, 10)); 
+		//p2.setLayout(new FlowLayout(1,0,0));
+		
+		for (StorageIngredient si : sIngredientList) {
+			JTextField listName = new JTextField(si.getName(),1);
+			JTextField listAmount = new JTextField(""+si.getAmount(),1);
+			p2.add(listName);
+			listName.setForeground(Color.RED);
+			listName.setFont(new Font("Verdana",Font.ITALIC,20));
+			listName.setBorder(BorderFactory.createEmptyBorder());
+			listName.setOpaque(false);
+			listName.setEditable(false);
+			
+			p2.add(listAmount);
+			listAmount.setForeground(Color.RED);
+			listAmount.setFont(new Font("Verdana",Font.ITALIC,20));
+			listAmount.setBorder(BorderFactory.createEmptyBorder());
+			listAmount.setOpaque(false);
+			listAmount.setEditable(false);
+		}
+		p.add(p2); 
+		
+		JPanel p3 = new JPanel();
+		p3.setLayout(new FlowLayout(1,10,10));
+		JButton Add = new JButton("Add"); 
+		Add.setContentAreaFilled(false);
+		Add.setFont(new Font("Verdana",Font.ITALIC,15));
+		Add.setOpaque(false);
+		
+		JButton update = new JButton("update"); 
+		update.setContentAreaFilled(false);
+		update.setFont(new Font("Verdana",Font.ITALIC,15));
+		update.setOpaque(false);
+		
+		JButton Mainpage = new JButton("Main page");
+		Mainpage.setContentAreaFilled(false);
+		Mainpage.setFont(new Font("Verdana",Font.ITALIC,15));
+		Mainpage.setOpaque(false);
+		
+		// Control the jump between pages (Maintain TO Add)
+		p3.add(Add);
+		Add.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Hidde the maintain Page
+				m.getView().get(0).setvisible(0);
+				IngredientMantainGUI.this.dispose();
+				// Show the add page
+				m.getView().get(2).setvisible(1);
+				try {
+					m.notifyView();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		// Control the jump between pages (Maintain TO update)
+		////////////////  UPDATE
+		p3.add(update);
+		update.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Hidde the maintain Page
+				m.getView().get(0).setvisible(0);
+				IngredientMantainGUI.this.dispose();
+				// Show the update page
+				m.getView().get(1).setvisible(1);
+				try {
+					m.notifyView();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		// Control the jump between pages (Maintain TO Main page)
+		p3.add(Mainpage);
+		Mainpage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				IngredientMantainGUI.this.dispose();
+			}
+		});
+		p.add(p3);
+		bg.add(emptyPanel);
+		bg.add(p);
 		this.add(bg);
 		
+		emptyPanel.setOpaque(false);
+		p3.setOpaque(false);
+		p2.setOpaque(false);
+		p1.setOpaque(false);
+		p.setOpaque(false);
 
 	}
 
